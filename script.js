2ac35405d21d7b271bd9be2c005f7cb16887c4fc
AICHOUN-C <cherif.aichoun@gmail.com>
2020-09-21 16:15:54 +0200

    Beta version

---
+++ script.js
@@ -0,0 +1,14 @@
+const name = prompt('Veuillez saisir votre nom');
+let randomNumber = Math.floor(Math.random() * 100) +1;
+let playerNumber;
+let i = 0;
+
+while (randomNumber != playerNumber) {
+    playerNumber = parseInt(prompt('Saisir un nombre entre 0 et 100'));
+    i++
+    if (playerNumber < randomNumber) {
+        console.log('Votre chiffre est plus petit que le chiffre mystère');
+    } else if (playerNumber > randomNumber) {
+        console.log('Votre chiffre est plus grand que le chiffre mystère');
+    } else alert(`Bravo ${name} vous avez trouvé le chiffre mystère en ${i} essai(s)`);
+ }
